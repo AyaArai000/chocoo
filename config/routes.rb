10c482439d60d532ac8475da0f49851de275c506
“Aya <eb210111@meiji.ac.jp>
2024-02-11 15:08:04 +0900

    node

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   get 'sweets/top' => 'sweets#top'
   get 'sweets/:sweet_id/likes' => 'likes#create'
   get 'sweets/:sweet_id/likes/:id' => 'likes#destroy'
-
-  resources :users, only: [:show]
+  devise_scope :user do
+    get 'users/sign_out' =>'devise/sessions#destroy'
+  end
+  resources :users, only: [:show, :destroy]
 
   resources :sweets do
     resources :likes, only: [:create, :destroy]
